446acbe13fdc384c881dee438e5895ae1544e804
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-04-08 20:54:50 -0300

    Detalhar Usuário

--- src/main/java/com/person/api_user/controller/UserController.java
+++ src/main/java/com/person/api_user/controller/UserController.java
@@ -4,10 +4,12 @@ import com.person.api_user.domain.user.model.User;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.person.api_user.domain.user.repository.UserRepository;
+import com.person.api_user.httpExceptions.UserNotFoudException;
 
 @RestController
 @RequestMapping("/user")
@@ -15,6 +17,12 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    public User detail(@PathVariable Integer id){
+        return userRepository.findById(id)
+        .orElseThrow(()-> new UserNotFoudException());
+    }
 
     @GetMapping
     public List<User> listAll(){
